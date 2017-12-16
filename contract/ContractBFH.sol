160faec82a6ee364b6c3b73cdd9bda2e3f885cea
Vadim <komyshenets@mail.ru>
2017-12-16 10:36:39 +0300

    Положить средства на контракт

--- contract/ContractBFH.sol
+++ contract/ContractBFH.sol
@@ -12,7 +12,12 @@ contract BFHChecker{
 
 
 
+/**
+ * Положить средства на контракт
+ */
     function putEth(address user, uint128 value) public payable{
-
+     mapping[user] = value;
     }
+
+
 }
